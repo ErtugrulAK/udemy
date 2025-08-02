45e84a60d640bc8d9f4a170b25abc0d91aa4051f
Ertuğrul AK <ertugrul14ak@gmail.com>
2025-08-02 17:51:21 +0300

    react inline styling

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,24 +1,18 @@
 import React from "react";
 import "./MyComponent.css";
 
-const img = "https://picsum.photos/200"
-
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black",
+};
+customStyle.color = "blue";
 
 function MyComponent() {
   return (
-    <div>
-      <h1 className="heading">My Favourite Foods</h1>
-      
-      <div>
-        <img src={img} />
+    
+    <h1 style={customStyle}>My Favourite Foods</h1>
 
-        <img className="food" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQujx5jb6WvhHb7XcW46Rmel1lts4KHWkuBfw&s" />
-        <img className="food" src="https://i.lezzet.com.tr/images-xxlarge-secondary/mukemmel-et-nasil-pisirilir-314a2432-6096-47e3-95c8-fa3e5a311da5.jpg" />
-        <img className="food" src="https://i.lezzet.com.tr/images-xxlarge-secondary/taze-meyve-ve-sebze-nasil-secilir-e0009203-a2fb-46be-990f-dbeafe11ed9f.jpg" />
-
-      </div>
-      
-    </div>
   );
 }
 
