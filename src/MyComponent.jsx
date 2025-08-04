d9acdcad2a1722e6e858ca96095386485ad58b6f
Ertuğrul AK <ertugrul14ak@gmail.com>
2025-08-04 14:36:25 +0300

    react keeper app part-1

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import "./MyComponent.css";
-import { add, multiply, subtract, divide} from "./calculator";
-
+import Heading from "./heading";
+import Footer from "./Footer";
+import Note from "./Note";
 
 function MyComponent() {
   return (
-    
-    <ul>
-    <li>{add(1, 2)}</li>
-    <li>{multiply(2, 3)}</li>
-    <li>{subtract(7, 2)}</li>
-    <li>{divide(5, 2)}</li>
-    </ul>
-    
+    <div>
+      <Heading />
+      <Footer />
+      <Note />
+    </div>
+
 
   );
 }
 
 export default MyComponent;
+
+
 
 
