519f8a6021699fccff22808cad95c17d02ea4841
Ertuğrul AK <ertugrul14ak@gmail.com>
2025-08-04 14:03:40 +0300

    react components practise

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import "./MyComponent.css";
 import Heading from "./heading";
-import Ul from "./ul";
 
 
 function MyComponent() {
   return (
     
-
+    <Heading />
     
-
-
-
-
 
   );
 }
@@ -20,10 +15,3 @@ function MyComponent() {
 export default MyComponent;
 
 
-//Create a React app from scratch.
-//Show a single h1 that says "Good morning" if between midnight and 12PM.
-//or "Good Afternoon" if between 12PM and 6PM.
-//or "Good evening" if between 6PM and midnight.
-//Apply the "heading" style in the styles.css
-//Dynamically change the color of the h1 using inline css styles.
-//Morning = red, Afternoon = green, Night = blue.
