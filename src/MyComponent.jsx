7356069928bc15909bcabfdf09dc3f275b17cde1
Ertuğrul AK <ertugrul14ak@gmail.com>
2025-08-04 14:17:37 +0300

    react import export with practise

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./MyComponent.css";
-import Heading from "./heading";
+import { add, multiply, subtract, divide} from "./calculator";
 
 
 function MyComponent() {
   return (
     
-    <Heading />
+    <ul>
+    <li>{add(1, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{subtract(7, 2)}</li>
+    <li>{divide(5, 2)}</li>
+    </ul>
     
 
   );
