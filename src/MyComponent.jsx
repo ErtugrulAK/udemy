db46cf02239c44f786cf209ac171fe28f6e1f109
Ertuğrul AK <ertugrul14ak@gmail.com>
2025-08-05 21:42:53 +0300

    react map-filter-reduce

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,30 +1,127 @@
 import React from "react";
 import "./MyComponent.css";
-import Entry from "./Entry";
 import emojipedia from "./emojipedia";
 
 
-function createEntry(emojiTerm) {
-  return (
-    <Entry
-      key={emojiTerm.id}
-      emoji={emojiTerm.emoji}
-      name={emojiTerm.name}
-      description={emojiTerm.meaning}
-    />
-  );
-}
+
+
+var numbers = [3, 56, 2, 48, 5];
+
+
+//----------------------------------------------------------------------------------
+//Map -Create a new array by doing something with each item in an array.
+//----------------------------------------------------------------------------------
+
+
+//function double (x){
+//  return x*2;
+//
+//}
+//const newNumbers= numbers.map(double);
+
+
+
+//var newNumbers = [];
+//
+//numbers.forEach(function double (x){
+//  newNumbers.push(x*2)
+//});
+
+
+
+//var newNumbers = [];
+//
+//numbers.map(function double (x){
+//  newNumbers.push(x*2)
+//});
+
+
+//----------------------------------------------------------------------------------
+//Filter - Create a new array by keeping the items that return true.
+//----------------------------------------------------------------------------------
+
+
+
+//const newNumbers = numbers.filter(function (num) {
+//  return num < 10
+//}
+//
+//);
+
+
+
+//var newNumbers = [];
+//
+//numbers.forEach(function (num){
+//  if (num < 10)
+//  {
+//    newNumbers.push(num);
+//  }
+//});
+
+
+
+
+
+//----------------------------------------------------------------------------------
+//Reduce - Accumulate a value by doing something to each item in an array.
+//----------------------------------------------------------------------------------
+
+
+//var newNumber = 0;
+//numbers.forEach(function (currentNumber) {
+//  newNumber += currentNumber
+//})
+
+
+//var newNumber = numbers.reduce(function (accumulator, currentNumber) {
+//    return accumulator + currentNumber;
+//})
+
+
+
+
+
+//----------------------------------------------------------------------------------
+//Find - find the first item that matches from an array.
+//----------------------------------------------------------------------------------
+
+
+
+
+//const newNumber = numbers.find(function (num) {
+//  return num > 10;
+//})
+
+
+
+
+//----------------------------------------------------------------------------------
+//FindIndex - find the index of the first item that matches.
+//----------------------------------------------------------------------------------
+
+
+//const newNumber = numbers.findIndex(function (num) {
+//  return num > 10;
+//})
+
+const max100char = emojipedia.map(function (x){
+  return x.meaning.substring(0, 100)
+});
+
+
+
 
 function MyComponent() {
   return (
-    <div>
-      <h1>
-        <span>emojipedia</span>
-      </h1>
-      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
-    </div>
+    <div>{max100char.join(", ")}</div>
+    //<div>{newNumber}</div>
   );
 }
 
 export default MyComponent;
+
+
+
+
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import MyComponent from './MyComponent';
 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <MyComponent />
-  </StrictMode>,
-)
+//createRoot(document.getElementById('root')).render(
+//  <StrictMode>
+//    <MyComponent />
+//  </StrictMode>,
+//)
+
+import emojipedia from "./emojipedia";
+
+const newEmojipedia = emojipedia.map(function (emojiEntry) {
+  return emojiEntry.meaning.substring(0, 100);
+});
+
+console.log(newEmojipedia);
