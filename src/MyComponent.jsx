b572e6a1379acd018e4306eb02c0248de22d6270
Ertuğrul AK <ertugrul14ak@gmail.com>
2025-08-05 16:24:53 +0300

    react mapping practice

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,49 +1,27 @@
 import React from "react";
 import "./MyComponent.css";
-import Card from "./Card";
-import contacts from "./contacts";
-import Avatar from "./Avatar";
+import Entry from "./Entry";
+import emojipedia from "./emojipedia";
 
 
-function createCard(contact) {
+function createEntry(emojiTerm) {
   return (
-    <Card
-      key={contact.id}
-      name={contact.name}
-      img={contact.imgURL}
-      tel={contact.phone}
-      email={contact.email}
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
     />
   );
 }
 
-
-
 function MyComponent() {
-
   return (
     <div>
-      <h1 className="heading">My Contacts</h1>
-      {contacts.map(createCard)}
-
-      {/* <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      /> */}
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
     </div>
   );
 }
