f9b6d40f7c52060b2e5596dad1176ad8fae2b6c3
Ertuğrul AK <ertugrul14ak@gmail.com>
2025-08-16 12:09:02 +0300

    react Object & Array Destructuring with challenge

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,23 +1,38 @@
 import React, { useState } from "react";
 import "./MyComponent.css";
+import cars from "./practice"
+
+console.log(cars);
+const [honda , tesla] = cars;
+
+const {speedStats: {topSpeed: hondaTopSpeed}} = honda;
+const {speedStats: {topSpeed: teslaTopSpeed}} = tesla;
+
+const {coloursByPopularity: [hondaTopColour]} = honda;
+const {coloursByPopularity: [teslaTopColour]} = tesla;
+
 
 function MyComponent() {
-  setInterval(latestTime, 10000);
-
-  const time = new Date().toLocaleTimeString();
-
-  const [newTime , setTime] = useState(time);
-
-  function latestTime() 
-  {
-    const timeLatest = new Date().toLocaleTimeString();
-    setTime(timeLatest);
-  }
+  
   return (
-    <div className="container">
-      <h1>{time}</h1>
-      <button onClick={latestTime}>Get Time</button>
-    </div>
+    <table>
+     <tr>
+       <th>Brand</th>
+       <th>Top Speed</th>
+       <th>Top Colour</th>
+     </tr>
+     <tr>
+       <td>{tesla.model}</td>
+       <td>{teslaTopSpeed}</td>
+       <td>{teslaTopColour}</td>
+     </tr>
+     <tr>
+       <td>{honda.model}</td>
+       <td>{hondaTopSpeed}</td>
+       <td>{hondaTopColour}</td>
+     </tr>
+   </table>
+    
   );
 }
 
