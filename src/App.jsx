2fbe1543ae18034b9377f4418730a85c86305259
Ertuğrul AK <ertugrul14ak@gmail.com>
2025-08-17 13:06:11 +0300

    react class components vs. functional components

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,14 @@
 import "./App.css";
 
-import React, { useState } from "react";
+import React from "react";
+import ClassComponent from "./ClassComponent";
+import FunctionalComponent from "./FunctionalComponent";
 
-function App() {
-  const [name, setName] = useState("");
-  const [headingText, setHeading] = useState("");
-
-  function handleChange(event) {
-    console.log(event.target.value);
-    setName(event.target.value);
+class App extends React.Component {
+  render() {
+    return <FunctionalComponent />;
   }
-
-  function handleClick(event) {
-    setHeading(name);
-
-    event.preventDefault();
-  }
-
-  return (
-    <div className="container">
-      <h1>Hello {headingText}</h1>
-      <form onSubmit={handleClick}>
-        <input
-          onChange={handleChange}
-          type="text"
-          placeholder="What's your name?"
-          value={name}
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
 }
 
 export default App;
+
